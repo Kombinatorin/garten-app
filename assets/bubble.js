7c5ffe9de695b6bea5d02394299b74c21b43b4c7
Kombinatorin <instructorstulle@googlemail.com>
2025-08-25 18:00:39 +0200

    Update bubble.js

--- assets/bubble.js
+++ assets/bubble.js
@@ -1,1 +1,10 @@
+// Simple bubble "pop" animation
+document.addEventListener("click", function(e) {
+  if(e.target.tagName === "BUTTON") {
+    e.target.style.transform = "scale(0.8)";
+    setTimeout(() => {
+      e.target.style.transform = "scale(1)";
+    }, 200);
+  }
+});
 
